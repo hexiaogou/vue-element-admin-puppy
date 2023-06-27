e5340bc02241501ade4e94a8df1ec2825c7a13bb
puppy-he <hexiaogouzhoujie@outlook.com>
2023-06-27 16:07:09 +0800

    add puppy favicon.ico

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    puppy
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,11 @@
 module.exports = {
     devServer: {
         port: 9527,
+        open: true,
         before: require('./mock/mock-server.js')
+    },
+    configureWebpack: {
+        name: 'puppy'
     }
 }
 
