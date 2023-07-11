4b0c64d035cb6b2966876d105318a2350934a4e7
puppy-he <hexiaogouzhoujie@outlook.com>
2023-07-11 15:34:02 +0800

    add 401 router and page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 
+import Element from 'element-ui'
 import '@/styles/index.scss'
 
 import router from './router'
 
 import './permission'
+
+Vue.use(Element)
 
 new Vue({
     el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ export const constantRoutes = [
         path: '/404',
         component: () => import('@/views/error-page/404'),
         hidden: true
+    },
+    {
+        path: '/401',
+        component: () => import('@/views/error-page/401'),
+        hidden: true
     }
 ]
 
