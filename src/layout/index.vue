d7ea3544809eef9dfe123530d5607ae96732fa20
puppy-he <hexiaogouzhoujie@outlook.com>
2023-07-20 10:35:51 +0800

    add layout in src

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>Puppy</div>
+  <div :class="classObj" class="app-wrapper">Puppy</div>
 </template>
 
 <script>
-
+import { mapState } from 'vuex'
+export default {
+  name: 'Layout',
+  computed: {
+    ...mapState({
+      sidebar: state => state.app.sidebar,
+      device: state => state.app.device,
+      showSettings: state => state.settings.showSettings,
+      needTagsView: state => state.settings.tagsView,
+      fixedHeader: state => state.settings.fixedHeader
+    }),
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile'
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
+  .app-wrapper {
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
 </style>
