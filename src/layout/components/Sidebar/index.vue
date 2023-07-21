2572eb98bb59161bf99d6b9a5d349ecd44e07c97
puppy-he <hexiaogouzhoujie@outlook.com>
2023-07-21 08:37:38 +0800

    add menu in sidebar

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,14 +1,30 @@
 <template>
   <div :class="{'has-logo':showLogo}">
-    <logo v-if="showLogo" :collapse="isCollapse" />
+    <logo v-if="showLogo" :collapse="isCollapse"/>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :background-color="variables.menuBg"
+          :text-color="variables.menuText"
+          :unique-opened="false"
+          :active-text-color="variables.menuActiveText"
+          :collapse-transition="false"
+          mode="vertical"
+      >
+
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 import Logo from './Logo'
+import variables from '@/styles/variables.scss'
+
 export default {
-  components:{
+  components: {
     Logo
   },
   computed: {
@@ -16,8 +32,20 @@ export default {
       'permission_routes',
       'sidebar'
     ]),
+    activeMenu() {
+      const route = this.$route
+      const { meta, path } = route
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu
+      }
+      return path
+    },
     showLogo() {
       return this.$store.state.settings.sidebarLogo
+    },
+    variables() {
+      return variables
     },
     isCollapse() {
       return !this.sidebar.opened
